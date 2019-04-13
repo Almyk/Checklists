fc7cce8b2ed126433aa57abe2ecb3652f5c807fc
almyk <hansentommy92@gmail.com>
2019-04-14 01:48:44 +0900

    Create new class ChecklistItem

---
+++ Checklist1/ChecklistItem.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistItem.swift
+//  Checklist1
+//
+//  Created by Tommy Hansen on 14/04/2019.
+//  Copyright © 2019 Tommy Hansen. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    func toggleChecked() {
+        checked = !checked;
+    }
+}
