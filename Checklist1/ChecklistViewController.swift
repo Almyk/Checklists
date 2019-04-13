6638d0a71bb5ece40b025442c7cd6e13d35ef3ea
almyk <hansentommy92@gmail.com>
2019-04-13 20:54:56 +0900

    Add 100 rows and 5 unique labels

--- Checklist1/ChecklistViewController.swift
+++ Checklist1/ChecklistViewController.swift
@@ -16,11 +16,27 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        let rowIndex = indexPath.row % 5
+        
+        if rowIndex == 0 {
+            label.text = "Walk the dog!"
+        } else if rowIndex == 1 {
+            label.text = "Brush my teeth"
+        } else if rowIndex == 2 {
+            label.text = "Learn iOS development"
+        } else if rowIndex == 3 {
+            label.text = "Soccer practice"
+        } else if rowIndex == 4 {
+            label.text = "Eat ice cream"
+        }
         return cell
     }
 
