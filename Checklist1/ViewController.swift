203d5a4f7e7120dd9c01831e192dd67b6dd826b8
almyk <hansentommy92@gmail.com>
2019-04-13 18:51:32 +0900

    Initial Commit

---
+++ Checklist1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist1
+//
+//  Created by Tommy Hansen on 13/04/2019.
+//  Copyright © 2019 Tommy Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
