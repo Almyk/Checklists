094ea008f6dafd1dd7d5dd2081d3787e80cb74e0
almyk <hansentommy92@gmail.com>
2019-04-14 18:25:04 +0900

    Add functionality to only enable done button when the text field is non empty

--- Checklist1/AddItemViewController.swift
+++ Checklist1/AddItemViewController.swift
@@ -8,23 +8,27 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         textField.becomeFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,
+                                                  with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
     }
     
     // MARK: - Actions
