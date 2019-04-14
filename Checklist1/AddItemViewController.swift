1b62649df1e7f7a8005a16e5925271798ad4a1d7
almyk <hansentommy92@gmail.com>
2019-04-14 17:07:31 +0900

    Add functions to cancel and add checklists

--- Checklist1/AddItemViewController.swift
+++ Checklist1/AddItemViewController.swift
@@ -19,6 +19,15 @@ class AddItemViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    // MARK: - Actions
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
 
     // MARK: - Table view data source
 
