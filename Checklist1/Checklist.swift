067436327f9bb95592f8073d215d7c1d05e40144
almyk <hansentommy92@gmail.com>
2019-04-15 02:32:22 +0900

    Add new class Checklist

---
+++ Checklist1/Checklist.swift
@@ -0,0 +1,18 @@
+//
+//  Checklist.swift
+//  Checklist1
+//
+//  Created by Tommy Hansen on 15/04/2019.
+//  Copyright © 2019 Tommy Hansen. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+    var name = ""
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+}
